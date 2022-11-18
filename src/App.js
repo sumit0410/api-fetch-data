a720d5bef66fbf63b03380fcc2e68498d6f77a7d
suraj53 <surajcsgo@gmail.com>
2022-11-18 16:12:39 +0530

    my first react-project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from 'react'
 
-export default App;
+export default class App extends Component {
+
+   state = {
+    postList: []
+   }
+
+   componentDidMount(){
+                axios.get("https://dummyjson.com/quotes")
+                .then((response)=>{
+                  console.log(response.data.quotes);
+                  this.setState({
+                    postList:response.data.quotes
+                  })
+                })
+                .catch((error)=>{
+                  console.log(error)
+                })
+   }
+  render() {
+    return (
+      <>
+        <h1>Fetching the data from API</h1>
+     
+        <>
+        <table border={1}>
+          <tr className='head-row'>
+            <th>Sno.</th>
+            <th>Quote</th>
+            <th>Author</th>
+          </tr>
+          {this.state.postList.map((item)=>(
+          <tr className='data-row'>
+            <td className='first-col'>{item.id}</td>
+            <td className='second-col'>{item.quote}</td>
+            <td className='third-col'>{item.author}</td>
+          </tr>
+      ))}
+
+        </table>
+        {/* <div key={item.id} >
+          <h3 style={{color:"red", display:"flex", flexDirection:"row"}}>{item.title}</h3>
+          <p>{item.body}</p>
+        </div> */}
+        </>
+      </>
+    )
+  }
+}
